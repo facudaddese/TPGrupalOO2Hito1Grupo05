bd3b3239a5b78a13519492b90caf11594d7d3b0a
Facundo D'addese <facundo.daddese19@gmail.com>
2026-08-23 01:16:22 -0300

    se agregan validaciones en StaffABM

--- src/negocio/StaffABM.java
+++ src/negocio/StaffABM.java
@@ -16,17 +16,27 @@ public class StaffABM {
     }
 
     public int agregar(Staff s) throws Exception {
+        if (dao.traerPorDni(s.getDni()) != null)
+            throw new Exception("ERROR: ya existe un integrante del Staff con el mismo DNI " + s.getDni());
+
         int edad = Period.between(s.getFechaNacimiento(), LocalDate.now()).getYears();
         if (edad < 18) throw new Exception("ERROR: el personal debe ser mayor de edad");
+
         return dao.agregar(s);
     }
 
-    public void modificar(Staff s) {
+    public void modificar(Staff s) throws Exception {
+        Staff existe = dao.traerPorDni(s.getDni());
+        if (existe != null && existe.getId() != s.getId())
+            throw new Exception("ERROR: ya existe un integrante del Staff con el mismo DNI " + s.getDni());
+
         dao.actualizar(s);
     }
 
-    public void eliminar(long id) {
+    public void eliminar(long id) throws Exception {
         Staff s = dao.traer(id);
+        if(s == null)
+            throw new Exception("ERROR: no existe personal con dicho DNI");
         dao.eliminar(s);
     }
 
