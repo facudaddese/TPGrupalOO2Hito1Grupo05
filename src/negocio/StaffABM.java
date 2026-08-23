633b17163d1042c6ea3cba6d231e5812a845b2de
Facundo D'addese <facundo.daddese19@gmail.com>
2026-08-23 00:55:01 -0300

    se crea la clase StaffABM que corresponde a la logica de negocio

--- src/negocio/StaffABM.java
+++ src/negocio/StaffABM.java
@@ -1,4 +1,36 @@
 package negocio;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+
+import dao.StaffDao;
+import datos.Staff;
+
 public class StaffABM {
+
+    StaffDao dao = new StaffDao();
+
+    public Staff traer(long id) {
+        return dao.traer(id);
+    }
+
+    public int agregar(Staff s) throws Exception {
+        int edad = Period.between(s.getFechaNacimiento(), LocalDate.now()).getYears();
+        if (edad < 18) throw new Exception("ERROR: el personal debe ser mayor de edad");
+        return dao.agregar(s);
+    }
+
+    public void modificar(Staff s) {
+        dao.actualizar(s);
+    }
+
+    public void eliminar(long id) {
+        Staff s = dao.traer(id);
+        dao.eliminar(s);
+    }
+
+    public List<Staff> traer() {
+        return dao.traer();
+    }
 }
--- src/test/TestAgregarStaff.java
+++ src/test/TestAgregarStaff.java
@@ -2,25 +2,25 @@ package test;
 
 import datos.Cocinero;
 import datos.Cajero;
-import dao.StaffDao;
+import negocio.StaffABM;
 
 import java.time.LocalDate;
 
 public class TestAgregarStaff {
-    public static void main(String[] args) {
-        StaffDao dao = new StaffDao();
+    public static void main(String[] args) throws Exception {
+        StaffABM abm = new StaffABM();
 
         Cocinero c = new Cocinero("Juan", "Pérez", 30111222,
                 LocalDate.of(1990, 5, 10),
                 LocalDate.of(2023, 1, 15),
                 350000, "Parrillero", 50000);
-        dao.agregar(c);
+        abm.agregar(c);
 
         Cajero ca = new Cajero("Ana", "Gómez", 28555111,
                 LocalDate.of(1995, 3, 20),
                 LocalDate.of(2024, 2, 1),
                 300000, "mañana");
-        dao.agregar(ca);
+        abm.agregar(ca);
 
         System.out.println("Datos insertados");
     }
