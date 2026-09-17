209d3698a0a0c4eb4aad4a950114c02e8d9d74fb
Facundo D'addese <facundo.daddese19@gmail.com>
2026-09-17 16:32:39 -0300

    se agregan las funciones de eliminar y actualizar cajero y cocinero

--- src/negocio/StaffABM.java
+++ src/negocio/StaffABM.java
@@ -7,6 +7,7 @@ import java.util.List;
 import dao.StaffDao;
 import datos.Cajero;
 import datos.Cocinero;
+import datos.Costo;
 import datos.Staff;
 
 public class StaffABM {
@@ -30,6 +31,29 @@ public class StaffABM {
         return dao.agregar(c);
     }
 
+    public void eliminarCocinero(int dni) throws Exception {
+        if (dao.traerPorDni(dni) == null)
+            throw new Exception("ERROR: no existe el cocinero con DNI " + dni);
+
+        if (!(dao.traerPorDni(dni) instanceof Cocinero))
+            throw new Exception("ERROR: el DNI " + dni + " no corresponde a un cocinero");
+
+        dao.eliminar(dao.traerPorDni(dni));
+    }
+
+    public void actualizarCocinero(int dni, String nuevaEspecialidad) throws Exception {
+        if (dao.traerPorDni(dni) == null)
+            throw new Exception("ERROR: no existe el cocinero con DNI " + dni);
+
+        if (!(dao.traerPorDni(dni) instanceof Cocinero))
+            throw new Exception("ERROR: el DNI " + dni + " no corresponde a un cocinero");
+
+        Cocinero co = (Cocinero) dao.traerPorDni(dni);
+        co.setEspecialidadCulinaria(nuevaEspecialidad);
+
+        dao.actualizar(dao.traerPorDni(dni));
+    }
+
     public int agregarCajero(String nombre, String apellido, int dni, LocalDate fechaNacimiento, LocalDate fechaIngreso, int sueldo, String turno) throws Exception {
         if (dao.traerPorDni(dni) != null)
             throw new Exception("ERROR: ya existe un integrante del Staff con el mismo DNI " + dni);
@@ -37,6 +61,29 @@ public class StaffABM {
         Cajero ca = new Cajero(nombre, apellido, dni, fechaNacimiento, fechaIngreso, sueldo, turno);
 
         return dao.agregar(ca);
+    }
+
+    public void eliminarCajero(int dni) throws Exception {
+        if (dao.traerPorDni(dni) == null)
+            throw new Exception("ERROR: no existe el cajero con DNI " + dni);
+
+        if (!(dao.traerPorDni(dni) instanceof Cajero))
+            throw new Exception("ERROR: el DNI " + dni + " no corresponde a un cajero");
+
+        dao.eliminar(dao.traerPorDni(dni));
+    }
+
+    public void actualizarCajero(int dni, String nuevoTurno) throws Exception {
+        if (dao.traerPorDni(dni) == null)
+            throw new Exception("ERROR: no existe el cajero con DNI " + dni);
+
+        if (!(dao.traerPorDni(dni) instanceof Cajero))
+            throw new Exception("ERROR: el DNI " + dni + " no corresponde a un cajero");
+
+        Cajero ca = (Cajero) dao.traerPorDni((dni));
+        ca.setTurno(nuevoTurno);
+
+        dao.actualizar(ca);
     }
 
     public List<Cajero> traerCajerosPorTurnoMayoresDeEdad(String turno) {
